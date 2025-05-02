a95a3851438c7fc5a4dbe71046839dacf1811fbf
JacobKilburn <jacobson124@icloud.com>
2025-05-02 10:22:58 -0400

    top module and sub modules created

---
+++ DDR_example/DDR_example.runs/pll_synth_1/pll_stub.vhdl
@@ -0,0 +1,35 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2024.2.1 (win64) Build 5266912 Sun Dec 15 09:03:24 MST 2024
+-- Date        : Fri May  2 10:01:50 2025
+-- Host        : engr-d1450-007 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               C:/Users/jwk50866/Documents/GitHub/Nexys_A7_DDR_example/DDR_example/DDR_example.runs/pll_synth_1/pll_stub.vhdl
+-- Design      : pll
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tcsg324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity pll is
+  Port ( 
+    clk_mem : out STD_LOGIC;
+    clk_cpu : out STD_LOGIC;
+    resetn : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in : in STD_LOGIC
+  );
+
+  attribute CORE_GENERATION_INFO : string;
+  attribute CORE_GENERATION_INFO of pll : entity is "pll,clk_wiz_v6_0_15_0_0,{component_name=pll,use_phase_alignment=true,use_min_o_jitter=false,use_max_i_jitter=false,use_dyn_phase_shift=false,use_inclk_switchover=false,use_dyn_reconfig=false,enable_axi=0,feedback_source=FDBK_AUTO,PRIMITIVE=MMCM,num_out_clk=2,clkin1_period=10.000,clkin2_period=10.000,use_power_down=false,use_reset=true,use_locked=true,use_inclk_stopped=false,feedback_type=SINGLE,CLOCK_MGR_TYPE=NA,manual_override=false}";
+end pll;
+
+architecture stub of pll is
+  attribute syn_black_box : boolean;
+  attribute black_box_pad_pin : string;
+  attribute syn_black_box of stub : architecture is true;
+  attribute black_box_pad_pin of stub : architecture is "clk_mem,clk_cpu,resetn,locked,clk_in";
+begin
+end;
